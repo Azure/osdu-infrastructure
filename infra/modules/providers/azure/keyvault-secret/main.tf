f044b1cdea04ce82a8c6bf1c525baed213e221b6
Daniel Scholl <dascholl@microsoft.com>
2020-07-20 17:47:26 -0500

    Bug Fix Unit Test

--- infra/modules/providers/azure/keyvault-secret/main.tf
+++ infra/modules/providers/azure/keyvault-secret/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_key_vault_secret" "secret" {
   key_vault_id = var.keyvault_id
 }
 
-data "azurerm_key_vault_secret" "secret" {
+data "azurerm_key_vault_secret" "secrets" {
   count        = length(var.secrets)
   depends_on   = [azurerm_key_vault_secret.secret]
   name         = local.secret_names[count.index]
