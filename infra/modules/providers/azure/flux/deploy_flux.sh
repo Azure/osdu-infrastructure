e1a9ba481256f1180a9ba350134b87efbe8364e0
Daniel Scholl <dascholl@microsoft.com>
2020-07-24 18:15:46 -0500

    Updated Script

--- infra/modules/providers/azure/flux/deploy_flux.sh
+++ infra/modules/providers/azure/flux/deploy_flux.sh
@@ -22,6 +22,9 @@ function finish {
 }
 trap finish EXIT
 cd $TMP_DIR
+pwd
+
+exit
 
 # are we running on macOs
 IS_MACOS=0
@@ -172,7 +175,14 @@ if kubectl get secret $KUBE_SECRET_NAME -n $KUBE_NAMESPACE > /dev/null 2>&1; the
         exit 1
     fi
 
-    secret=$(< "$GITOPS_SSH_KEY" base64 -w 0)
+    if [ "$IS_MACOS" -eq "1" ]; then
+      # use sed compatible with MacOS
+      secret=$(< "$GITOPS_SSH_KEY" base64)
+    else
+      secret=$(< "$GITOPS_SSH_KEY" base64 -w 0)
+    fi
+
+    
     if ! kubectl patch secret $KUBE_SECRET_NAME -n $KUBE_NAMESPACE -p="{\"data\":{\"identity\": \"$secret\"}}"; then
         echo "ERROR: failed to patch existing flux secret: $KUBE_SECRET_NAME "
         exit 1
