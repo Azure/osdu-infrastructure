a06cc5f4c30915fa4744c442f43e93a5ff236610
Daniel Scholl <dascholl@microsoft.com>
2020-07-24 22:43:10 -0500

    Updated

--- infra/modules/providers/azure/flux/main.tf
+++ infra/modules/providers/azure/flux/main.tf
@@ -12,28 +12,34 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-data "template_file" "flux_script" {
-  template = "${file("${path.module}/deploy_flux.sh")}"
-
-  vars {
-    flux_recreate = var.flux_recreate
-  }
-}
-
-resource "null_resource" "script" {
+resource "null_resource" "deploy_flux" {
   count = var.enable_flux ? 1 : 0
 
-  # Trigger when the script when variables change
   triggers = {
+    enable_flux   = var.enable_flux
     flux_recreate = var.flux_recreate
-    script_sha    = "${sha256(file("${path.module}/deploy_flux.sh"))}"
   }
 
   provisioner "local-exec" {
-    command     = "${data.template_file.flux_script.rendered}"
-    interpreter = ["/bin/bash", "-c"]
+    command = <<EOT
+     echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfig_complete};
+     KUBECONFIG=${var.output_directory}/${var.kubeconfig_filename} ${path.module}/deploy_flux.sh \
+      -b '${var.gitops_url_branch}' \
+      -f '${var.flux_repo_url}' \
+      -g '${var.gitops_ssh_url}' \
+      -k '${var.gitops_ssh_key_path}' \
+      -d '${var.flux_clone_dir}' \
+      -c '${var.gitops_poll_interval}' \
+      -l '${var.gitops_label}' \
+      -e '${var.gitops_path}' \
+      -s '${var.acr_enabled}' \
+      -r '${var.flux_image_repository}' \
+      -t '${var.flux_image_tag}' \
+      -z '${var.gc_enabled}'
+EOT
   }
 }
+
 
 # resource "null_resource" "deploy_flux" {
 #   count = var.enable_flux ? 1 : 0
