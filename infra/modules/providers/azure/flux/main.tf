a3c9ce0d9b38b89f97a1d36b326f6fc4b69635d7
Daniel Scholl <dascholl@microsoft.com>
2020-07-24 23:16:15 -0500

    Updated

--- infra/modules/providers/azure/flux/main.tf
+++ infra/modules/providers/azure/flux/main.tf
@@ -22,7 +22,8 @@ resource "null_resource" "deploy_flux" {
 
   provisioner "local-exec" {
     command     = <<EOT
-     echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfig_complete};
+     ls -l ${path.module}
+     echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfig_complete}
      KUBECONFIG=${var.output_directory}/${var.kubeconfig_filename} ${path.module}/deploy_flux.sh \
       -b '${var.gitops_url_branch}' \
       -f '${var.flux_repo_url}' \
@@ -37,7 +38,6 @@ resource "null_resource" "deploy_flux" {
       -t '${var.flux_image_tag}' \
       -z '${var.gc_enabled}'
 EOT
-    interpreter = ["/bin/bash"]
   }
 }
 
