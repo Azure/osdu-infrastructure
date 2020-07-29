cd921a571baaf8a70ca0b6777849d96ee0cfa79b
NickKarsky <nkarsky@terpmail.umd.edu>
2020-07-29 15:17:25 -0400

    Feature/kv unit tests (#86)
    * Added Unit test for container-registry module
    
    * Got rid of null assertion to fix test
    
    * Added unit tests for keyvault module
    
    * got rid of excess files and formatted
    
    * Delete main.tf
    
    * Delete unit_test.go
    
    Co-authored-by: Nick Karsky <nikarsky@microsoft.com>

---
+++ infra/modules/providers/azure/keyvault/testing/unit_test.go
@@ -0,0 +1,87 @@
+//  Copyright © Microsoft Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/microsoft/cobalt/test-harness/infratests"
+)
+
+var name = "keyvault-"
+var location = "eastus"
+var count = 5
+
+var tfOptions = &terraform.Options{
+	TerraformDir: "./",
+	Upgrade:      true,
+}
+
+func asMap(t *testing.T, jsonString string) map[string]interface{} {
+	var theMap map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonString), &theMap); err != nil {
+		t.Fatal(err)
+	}
+	return theMap
+}
+
+func TestTemplate(t *testing.T) {
+
+	expectedKeyVault := asMap(t, `{
+		"name" : "spkeyvault",
+		"resource_group_name" : "osdu-module",
+		"sku_name" : "standard",
+		"tags" : {
+			"osdu" : "module"
+		}
+	}`)
+
+	expectedAccessPolicy := asMap(t, `{
+		"certificate_permissions" : [
+			"create",
+			"delete",
+			"get",
+			"list"
+		],
+		"key_permissions" : [
+			"create",
+			"delete",
+			"get"
+		],
+		"secret_permissions" : [
+			"set",
+			"delete",
+			"get",
+			"list"
+		]
+	}`)
+
+	testFixture := infratests.UnitTestFixture{
+		GoTest:                t,
+		TfOptions:             tfOptions,
+		Workspace:             name + random.UniqueId(),
+		PlanAssertions:        nil,
+		ExpectedResourceCount: count,
+		ExpectedResourceAttributeValues: infratests.ResourceDescription{
+			"module.keyvault.module.deployment_service_principal_keyvault_access_policies.azurerm_key_vault_access_policy.keyvault[0]": expectedAccessPolicy,
+			"module.keyvault.azurerm_key_vault.keyvault": expectedKeyVault,
+		},
+	}
+
+	infratests.RunUnitTests(&testFixture)
+}
