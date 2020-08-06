4294a1ba00966dda880fee89e0918a439dcf2558
danielscholl <dascholl@microsoft.com>
2020-08-05 20:21:11 -0500

    Added Support for osdu pod identity to access secrets using CSI Secret Driver (#92)

--- infra/templates/osdu-r3-resources/environments/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-resources/environments/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           59,
+		ExpectedResourceCount:           61,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
