475e6e6b2b46bb2fcd93d61ee5c3ed9e68bb403e
Daniel Scholl <dascholl@microsoft.com>
2020-08-06 20:33:03 -0500

    Added Role for testing later

--- infra/templates/osdu-r3-resources/environments/service_resources/tests/integration/identity_roles.go
+++ infra/templates/osdu-r3-resources/environments/service_resources/tests/integration/identity_roles.go
@@ -87,10 +87,12 @@ func getAGICExpectedRoleAssignmentsMap(output infratests.TerraformOutput) map[st
 // Constructs the expected kubelet managed identity role assignments based off the Terraform output
 func getKubeletIdentityExpectedRoleAssignmentsMap(output infratests.TerraformOutput) map[string]string {
 	expectedAssignments := map[string]string{}
+	expectedAssignments[output["aks_node_resource_group_id"].(string)] = "Reader"
 	expectedAssignments[output["aks_node_resource_group_id"].(string)] = "Virtual Machine Contributor"
 	expectedAssignments[output["aks_node_resource_group_id"].(string)] = "Managed Identity Operator"
 	expectedAssignments[output["akspod_identity_id"].(string)] = "Managed Identity Operator"
 	expectedAssignments[output["agic_identity_id"].(string)] = "Managed Identity Operator"
+	expectedAssignments[output["aad_osdu_identity_id"].(string)] = "Managed Identity Operator"
 	expectedAssignments[output["container_registry_id"].(string)] = "AcrPull"
 
 	return expectedAssignments
