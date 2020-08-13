f48b124119c709099a0b78f2c4e1ce02b1813a67
danielscholl <dascholl@microsoft.com>
2020-08-13 15:14:18 -0500

    Added KEDA Support for Function Apps (#102)

--- infra/templates/osdu-r3-resources/environments/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-resources/environments/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           86,
+		ExpectedResourceCount:           88,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
