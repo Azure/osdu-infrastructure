0a97a4876e4831f78796e85d5c03e8d4720dced0
Daniel Scholl <dascholl@microsoft.com>
2020-08-24 14:42:28 -0500

    Updated Unit Test Counts

--- infra/templates/osdu-r3-resources/configurations/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-resources/configurations/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           90,
+		ExpectedResourceCount:           98,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
