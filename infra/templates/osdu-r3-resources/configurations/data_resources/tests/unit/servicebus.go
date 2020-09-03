535e4fe1824ccadc2b8f8f0d2b648738c63c6746
polavishnu4444 <hepola@microsoft.com>
2020-09-04 00:15:54 +0530

    fixed UTs

--- infra/templates/osdu-r3-resources/configurations/data_resources/tests/unit/servicebus.go
+++ infra/templates/osdu-r3-resources/configurations/data_resources/tests/unit/servicebus.go
@@ -21,25 +21,25 @@ import (
 
 func appendServicebusTests(t *testing.T, description infratests.ResourceDescription) {
 
-	description["module.service_bus.azurerm_servicebus_subscription.subscription[0]"] = asMap(t, `{
+	description["module.service_bus.azurerm_servicebus_subscription.subscription[4]"] = asMap(t, `{
 		"name":                                 "recordstopicsubscription",
 		"dead_lettering_on_message_expiration": true
 	}`)
 
-	description["module.service_bus.azurerm_servicebus_subscription.subscription[5]"] = asMap(t, `{
+	description["module.service_bus.azurerm_servicebus_subscription.subscription[3]"] = asMap(t, `{
 		"name":                                 "indexing-progresssubscription",
 		"dead_lettering_on_message_expiration": true,
 		"max_delivery_count":                   5
 	}`)
 
-	description["module.service_bus.azurerm_servicebus_topic.sptopic[0]"] = asMap(t, `{
+	description["module.service_bus.azurerm_servicebus_topic.sptopic[3]"] = asMap(t, `{
 		"name":                         "recordstopic",
 		"enable_partitioning":          false,
 		"requires_duplicate_detection": true,
 		"support_ordering":             true
 	}`)
 
-	description["module.service_bus.azurerm_servicebus_topic.sptopic[3]"] = asMap(t, `{
+	description["module.service_bus.azurerm_servicebus_topic.sptopic[2]"] = asMap(t, `{
 		"name":                         "indexing-progress",
 		"enable_partitioning":          false,
 		"requires_duplicate_detection": true,
