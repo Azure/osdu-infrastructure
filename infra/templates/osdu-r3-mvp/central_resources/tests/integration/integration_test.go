1554db98abd23ad436510e03bc7e0589c5c96d73
Daniel Scholl <dascholl@microsoft.com>
2020-09-15 09:56:27 -0500

    Updated

--- infra/templates/osdu-r3-mvp/central_resources/tests/integration/integration_test.go
+++ infra/templates/osdu-r3-mvp/central_resources/tests/integration/integration_test.go
@@ -38,11 +38,7 @@ func TestDataEnvironment(t *testing.T) {
 	testFixture := infratests.IntegrationTestFixture{
 		GoTest:                t,
 		TfOptions:             tfOptions,
-<<<<<<< HEAD
 		ExpectedTfOutputCount: 7,
-=======
-		ExpectedTfOutputCount: 6,
->>>>>>> master
 		TfOutputAssertions: []infratests.TerraformOutputValidation{
 			containerRegistryIntegTests.InspectContainerRegistryOutputs(subscription, "central_resource_group_name", "container_registry_name"),
 		},
