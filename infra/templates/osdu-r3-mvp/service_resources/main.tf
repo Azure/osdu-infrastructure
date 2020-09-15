900b5ed1602e798dddafa72502d7788cf916d2b8
Daniel Scholl <dascholl@microsoft.com>
2020-09-15 12:05:57 -0500

    Updated with providers and fixed eventgrid topic

--- infra/templates/osdu-r3-mvp/service_resources/main.tf
+++ infra/templates/osdu-r3-mvp/service_resources/main.tf
@@ -679,3 +679,36 @@ resource "azurerm_role_assignment" "agic_app_gw_mi" {
   scope                = module.appgateway.managed_identity_resource_id
   role_definition_name = "Managed Identity Operator"
 }
+
+
+
+#-------------------------------
+# Providers  (common.tf)
+#-------------------------------
+
+// Hook-up kubectl Provider for Terraform
+provider "kubernetes" {
+  version                = "~> 1.11.3"
+  load_config_file       = false
+  host                   = module.aks.kube_config.0.host
+  username               = module.aks.kube_config.0.username
+  password               = module.aks.kube_config.0.password
+  client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+}
+
+// Hook-up helm Provider for Terraform
+provider "helm" {
+  version = "~> 1.2.3"
+
+  kubernetes {
+    load_config_file       = false
+    host                   = module.aks.kube_config.0.host
+    username               = module.aks.kube_config.0.username
+    password               = module.aks.kube_config.0.password
+    client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(module.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
