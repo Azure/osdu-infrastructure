7bca6804bd512728449394f655939a0bd03626dc
danielscholl <dascholl@microsoft.com>
2020-09-17 17:22:51 -0500

    Updated for Airflow and fixed roles in partition (#173)

--- infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           74,
+		ExpectedResourceCount:           80,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
