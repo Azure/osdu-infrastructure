7be77a8c3a5caa02282c22616101f64915026797
Daniel Scholl <dascholl@microsoft.com>
2020-09-29 10:46:58 -0500

    Bug Fix unit test count for PR adding in more collections

--- infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           71,
+		ExpectedResourceCount:           75,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
