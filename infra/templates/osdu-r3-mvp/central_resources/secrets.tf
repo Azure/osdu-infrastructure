9541b618a792a412f6242f93316d2035a6fee1c5
danielscholl <dascholl@microsoft.com>
2020-10-01 13:11:21 -0500

    Added Keys to help pipelines (#193)
    * Added Keys to help pipelines
    
    * More keys

--- infra/templates/osdu-r3-mvp/central_resources/secrets.tf
+++ infra/templates/osdu-r3-mvp/central_resources/secrets.tf
@@ -25,14 +25,39 @@
 # Private Variables
 #-------------------------------
 locals {
-  ai_key_name = "appinsights-key"
-
   storage_account_name = format("tbl-storage")
   storage_key_name     = format("%s-key", local.storage_account_name)
 
   logs_id_name  = "log-workspace-id"
   logs_key_name = "log-workspace-key"
 }
+
+
+#-------------------------------
+# Misc
+#-------------------------------
+resource "azurerm_key_vault_secret" "base_name_21" {
+  name         = "base-name-21"
+  value        = local.base_name_21
+  key_vault_id = module.keyvault.keyvault_id
+}
+
+resource "azurerm_key_vault_secret" "resource_group" {
+  name         = "cr-resource-group"
+  value        = local.resource_group_name
+  key_vault_id = module.keyvault.keyvault_id
+}
+
+
+#-------------------------------
+# Container Registry
+#-------------------------------
+resource "azurerm_key_vault_secret" "container_registry_name" {
+  name         = "container-registry"
+  value        = module.container_registry.container_registry_name
+  key_vault_id = module.keyvault.keyvault_id
+}
+
 
 #-------------------------------
 # Storage
@@ -55,7 +80,7 @@ resource "azurerm_key_vault_secret" "storage_key" {
 # Application Insights
 #-------------------------------
 resource "azurerm_key_vault_secret" "insights" {
-  name         = local.ai_key_name
+  name         = "appinsights-key"
   value        = module.app_insights.app_insights_instrumentation_key
   key_vault_id = module.keyvault.keyvault_id
 }
@@ -96,5 +121,12 @@ resource "azurerm_key_vault_secret" "principal_secret" {
 resource "azurerm_key_vault_secret" "principal_object_id" {
   name         = "app-dev-sp-id"
   value        = module.service_principal.id
+  key_vault_id = module.keyvault.keyvault_id
+}
+
+// Add Application Information to KV
+resource "azurerm_key_vault_secret" "application_id" {
+  name         = "aad-client-id"
+  value        = module.ad_application.id
   key_vault_id = module.keyvault.keyvault_id
 }
